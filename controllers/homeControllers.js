dd7bf370e7d715bc8a2d4b4cbc1f9a99a6faea18
Deetsy <amardeepdeepak2016@gmail.com>
2024-10-03 19:14:24 +0530

    feat(handle url):Validate full URL

--- controllers/homeControllers.js
+++ controllers/homeControllers.js
@@ -1,15 +1,30 @@
 const queries = require("../prisma/queries");
 const short = require("short-uuid");
+const isUrl = require("is-url");
+//function to validate if a url is valid
+const isValid = (url) => {
+  return isUrl(url);
+};
 const postGenerateLinks = async (req, res) => {
   const fullURL = req.body.fullURL;
-  const shortURL = short.generate();
-  await queries.createLinks(fullURL, shortURL);
-  res.redirect("/");
+  if (isValid(fullURL)) {
+    const shortURL = short.generate();
+    await queries.createLinks(fullURL, shortURL);
+    res.redirect("/");
+  } else {
+    //return invalid url error
+    res.redirect("/generateLinks");
+  }
 };
 const postSubmitLinks = async (req, res) => {
   const fullURL = req.body.fullURL;
-  const shortURL = req.body.shortURL;
-  await queries.createLinks(fullURL, shortURL);
-  res.redirect("/");
+  if (isValid(fullURL)) {
+    const shortURL = req.body.shortURL;
+    await queries.createLinks(fullURL, shortURL);
+    res.redirect("/");
+  } else {
+    //return invalid url error
+    res.redirect("/submitLinks");
+  }
 };
 module.exports = { postGenerateLinks, postSubmitLinks };
